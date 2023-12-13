289a4268831b7ae34e0a27d6d94a1886ef784459
llymburn512 <llymburn@umich.edu>
2023-12-13 11:03:09 -0500

    Update CORA 2018 dependency link

--- download-dependencies.sh
+++ download-dependencies.sh
@@ -7,7 +7,7 @@ git clone --depth=1 https://github.com/ramvasudevan/RTD
 git clone --depth=1 https://github.com/skousik/RTD_tutorial
 
 # Install CORA
-wget https://tumcps.github.io/CORA/data/CORA_2018.zip && \
+wget https://tumcps.github.io/CORA/data/archive/version/CORA_2018.zip && \
   unzip CORA_2018.zip
 
 rm -f CORA_2018.zip  
